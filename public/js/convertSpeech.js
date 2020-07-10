cd4a8a0eb993aec701923bd93fd8cfdc4a2c3a62
tanisha7 <42581611+tanisha7@users.noreply.github.com>
2020-07-10 21:16:42 +0530

    Update convertSpeech.js

--- public/js/convertSpeech.js
+++ public/js/convertSpeech.js
@@ -13,12 +13,6 @@ var instructions = $('#recording-instructions');
 var notesList = $('ul#notes');
 var noteContent = '';
 
-// Get all notes from previous sessions and display them.
-var notes = getAllNotes();
-renderNotes(notes);
-
-
-
 /*-----------------------------
       Voice Recognition
 ------------------------------*/
@@ -96,105 +90,10 @@ $('#stop-record-btn').on('click', function(e) {
     recognition.stop();
 
     if (!noteContent.length) {
-        instructions.text('Could not save empty note. Please add a message to your note.');
+        instructions.text('Textbox is Empty. Please speak to add text.');
     } else {
-        // Save note to localStorage.
-        // The key is the dateTime with seconds, the value is the content of the note.
-        saveNote(new Date().toLocaleString(), noteContent);
-
-        // Reset variables and update UI.
-        noteContent = '';
-        renderNotes(getAllNotes());
-        noteTextarea.val('P: ');
-        instructions.text('Voice recognition stopped and Note saved successfully.');
+        instructions.text('Voice recognition stopped.');
     }
 
 
 });
-notesList.on('click', function(e) {
-    e.preventDefault();
-    var target = $(e.target);
-
-    // Listen to the selected note.
-    if (target.hasClass('listen-note')) {
-        var content = target.closest('.note').find('.content').text();
-        readOutLoud(content);
-    }
-
-    // Delete note.
-    if (target.hasClass('delete-note')) {
-        var dateTime = target.siblings('.date').text();
-        deleteNote(dateTime);
-        target.closest('.note').remove();
-    }
-});
-
-
-
-/*-----------------------------
-      Speech Synthesis
-------------------------------*/
-
-function readOutLoud(message) {
-    var speech = new SpeechSynthesisUtterance();
-
-    // Set the text and voice attributes.
-    speech.text = message;
-    speech.volume = 1;
-    speech.rate = 1;
-    speech.pitch = 3;
-
-    window.speechSynthesis.speak(speech);
-}
-
-
-
-/*-----------------------------
-      Helper Functions
-------------------------------*/
-
-function renderNotes(notes) {
-    var html = '';
-    if (notes.length) {
-        notes.forEach(function(note) {
-            html += `<li class="note">
-        <p class="header">
-          <span class="date">${note.date}</span>
-          <a href="#" class="listen-note" title="Listen to Note">Listen to Note</a>
-          <a href="#" class="delete-note" title="Delete">Delete</a>
-        </p>
-        <p class="content">${note.content}</p>
-      </li>`;
-        });
-    } else {
-        html = '<li><p class="content">You don\'t have any notes yet.</p></li>';
-    }
-    notesList.html(html);
-}
-
-
-function saveNote(dateTime, content) {
-    localStorage.setItem('note-' + dateTime, content);
-}
-
-
-function getAllNotes() {
-    var notes = [];
-    var key;
-    for (var i = 0; i < localStorage.length; i++) {
-        key = localStorage.key(i);
-
-        if (key.substring(0, 5) == 'note-') {
-            notes.push({
-                date: key.replace('note-', ''),
-                content: localStorage.getItem(localStorage.key(i))
-            });
-        }
-    }
-    return notes;
-}
-
-
-function deleteNote(dateTime) {
-    localStorage.removeItem('note-' + dateTime);
-}
